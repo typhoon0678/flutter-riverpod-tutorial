e236f8e2b551c3e5ac8813e0ed88fd8a43a83884
typhoon0678 <typhoon0678@gmail.com>
2025-06-11 05:06:17 +0900

    feat: 상태 변화 메서드 작성

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,30 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_files/models/product.dart';
+
+class CartNotifier extends Notifier<Set<Product>> {
+  // initial value
+  @override
+  Set<Product> build() {
+    return {
+      const Product(
+          id: '4',
+          title: 'Red Backpack',
+          price: 14,
+          image: 'assets/products/backpack.png'),
+    };
+  }
+
+  // methods to update state
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    state = state.where((p) => p.id != product.id).toSet();
+  }
+}
+
+final cartNotifierProvider =
+    NotifierProvider<CartNotifier, Set<Product>>(() => CartNotifier());
